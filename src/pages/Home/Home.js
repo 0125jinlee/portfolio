43e982bae7fb9f53a1b42195559901fb9795286d
Jin Won Lee <0125jinwonlee@gmail.com>
2020-10-07 21:41:04 -0700

    portfolio in progress

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,11 +5,10 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="Home">
-      <h1>HELLO.</h1>
-      <br />
-      <h1>THIS IS JIN WON LEE.</h1>
-      <br />
-      <h1>I WANT TO BE A FRONT-END DEVELOPER.</h1>
+      <div className="Title">
+      <h1 className="EnglishTitle">HELLO, I AM JIN WON LEE.</h1>
+      <h1 className="KoreanTitle">안녕하세요, 저는 이진원 입니다.</h1>
+      </div>
     </div>
   );
 };
